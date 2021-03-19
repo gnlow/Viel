cb95f6bcdebf3f4f9fb9707f7c87ba06b0ad4ed6
Gnlow <entrydark@gmail.com>
2021-03-19 20:31:56 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,14 +1,28 @@
+const funcs = {
+    더하기(a: number, b: number) {
+        return a + b
+    },
+    말하기(str: string) {
+        console.log(str)
+    }
+}
+
 type Josa = "에" | "를" | "을"
 
-type Parser <T extends string> = 
+type Tokenizer <T extends string> = 
     T extends `${infer Value} ${infer State}`
         ? [
             Value extends `${infer Word}${Josa}`
                 ? Word
                 : Value,
-            ...Parser<State>
+            ...Tokenizer<State>
         ]
         : [T]
 
+type FuncInputCount<name extends keyof typeof funcs> =
+    Parameters<(typeof funcs)[name]> extends {length: infer L}
+        ? L
+        : 0
 
-var a: Parser<"1에 2를 더하기를 말하기">
+var a: Tokenizer<"1에 2를 더하기를 말하기">
+var b: FuncInputCount<"더하기">
